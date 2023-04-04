04c4d3aea2a4ae4925ec14812f33fb1f6d846d64
dacoppock <129800076+dacoppock@users.noreply.github.com>
2023-04-04 00:36:43 +0000

    Added Flask Project

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def hello_world():
+  return "<p>Hello Douglas...</p>"
+
+print(__name__)
+if __name__ == '__main__':
+  print("in the if statement")
+  app.run(host='0.0.0.0' , debug=True)
